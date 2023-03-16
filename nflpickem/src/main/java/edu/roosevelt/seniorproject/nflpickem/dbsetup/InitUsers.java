64ddd54dc98b6a80fbab20b9538c24b2dd4fec4e
manduul0804 <manduul0804@gmail.com>
2023-03-16 03:19:35 -0500

    Merge Muhammad to Team A branch

--- nflpickem/src/main/java/edu/roosevelt/seniorproject/nflpickem/dbsetup/InitUsers.java
+++ nflpickem/src/main/java/edu/roosevelt/seniorproject/nflpickem/dbsetup/InitUsers.java
@@ -6,6 +6,8 @@ package edu.roosevelt.seniorproject.nflpickem.dbsetup;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  *
