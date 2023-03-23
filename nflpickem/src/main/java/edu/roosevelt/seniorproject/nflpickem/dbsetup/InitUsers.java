dae9892bd458157fa41a36194ce32f7bbd560587
manduul0804 <manduul0804@gmail.com>
2023-03-22 19:43:27 -0500

    Returned to the original main

--- nflpickem/src/main/java/edu/roosevelt/seniorproject/nflpickem/dbsetup/InitUsers.java
+++ nflpickem/src/main/java/edu/roosevelt/seniorproject/nflpickem/dbsetup/InitUsers.java
@@ -23,91 +23,56 @@ public class InitUsers {
     public static void main(String[] args) {
         // TODO code application logic here
         try {
-            Scanner scanner = new Scanner(new File("users.txt"));
+
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/nflpickem",
                     "user", "user");
 
             System.out.println("Create USER/GAME (bases)");
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                System.out.println("line: " + line);
-                Scanner lineScanner = new Scanner(line);
 
-                //Get data line by line and seperate it by ws
-                while (lineScanner.hasNext()) {
-                    String userName = lineScanner.next().trim();
-                    String password = lineScanner.next().trim();
-                    String admin = lineScanner.next().trim();
-                    String name = lineScanner.next().trim();
-                    String email = lineScanner.next().trim();
+            String[] uns = {"admin", "red", "blue", "green", "yellow"};
+//            //USER goes first
+//            String sql = "CREATE TABLE USER (";
+//            sql = sql + " USERNAME VARCHAR(30) PRIMARY KEY,";
+//            sql = sql + " PASSWORD VARCHAR(20),";
+//            sql = sql + " ADMIN BOOLEAN,";
+//            sql = sql + " NAME VARCHAR(50),";
+//            sql = sql + " EMAIL VARCHAR(50))";
 
-                    String sql = "INSERT INTO USER VALUES ('";
-                    sql = sql + userName + "','";
-                    sql = sql + password + "','";
-                    sql = sql + admin + "','";
-                    sql = sql + name + "','";
-                    sql = sql + email + "');";
+            for (int i = 0; i < uns.length; i++) {
+                String sql = "INSERT INTO USER VALUES ('";
+                sql = sql + uns[i] + "',";
+                sql = sql + "'" + uns[i] + "',";
+                if (i == 0) {
+                    sql = sql + "" + true + ",";
+                } else {
+                    sql = sql + "" + false + ",";
+                }
+                String name = "";
+                //even odd
+                if (i % 2 == 0) {
+                    name = "Mr. " + uns[i].substring(0, 1).toUpperCase() + uns[i].substring(1);
+                } else {
+                    name = "Ms. " + uns[i].substring(0, 1).toUpperCase() + uns[i].substring(1);
+                }
+                sql = sql + "'" + name + "',";
+                sql = sql + "'" + uns[i] + "@mail.roosevelt.edu" + "')";
 
-                    System.out.println(sql);
+                System.out.println(sql);
 
-                    try {
-                        conn.createStatement().execute(sql);
+                try {
+                    conn.createStatement().execute(sql);
 
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
 
             }
+
             System.out.println("Table USER created!");
         } catch (Exception e) {
             System.out.println("oops:" + e);
         }
-//            String[] uns = {"admin", "red", "blue", "green", "yellow"};
-////            //USER goes first
-////            String sql = "CREATE TABLE USER (";
-////            sql = sql + " USERNAME VARCHAR(30) PRIMARY KEY,";
-////            sql = sql + " PASSWORD VARCHAR(20),";
-////            sql = sql + " ADMIN BOOLEAN,";
-////            sql = sql + " NAME VARCHAR(50),";
-////            sql = sql + " EMAIL VARCHAR(50))";
-//
-//            for (int i = 0; i < uns.length; i++) {
-//                String sql = "INSERT INTO USER VALUES ('";
-//                sql = sql + uns[i] + "',";
-//                sql = sql + "'" + uns[i] + "',";
-//                if (i == 0) {
-//                    sql = sql + "" + true + ",";
-//                } else {
-//                    sql = sql + "" + false + ",";
-//                }
-//                String name = "";
-//                //even odd
-//                if (i % 2 == 0) {
-//                    name = "Mr. " + uns[i].substring(0, 1).toUpperCase() + uns[i].substring(1);
-//                } else {
-//                    name = "Ms. " + uns[i].substring(0, 1).toUpperCase() + uns[i].substring(1);
-//                }
-//                sql = sql + "'" + name + "',";
-//                sql = sql + "'" + uns[i] + "@mail.roosevelt.edu" + "')";
-//
-//                System.out.println(sql);
-//
-//                try {
-//                    conn.createStatement().execute(sql);
-//
-//                } catch (Exception e) {
-//                    System.out.println(e.getMessage());
-//                }
-//
-//            }
-//
-//            System.out.println("Table USER created!");
-//        } catch (Exception e) {
-//            System.out.println("oops:" + e);
-//        }
-//      
 
     }
 
