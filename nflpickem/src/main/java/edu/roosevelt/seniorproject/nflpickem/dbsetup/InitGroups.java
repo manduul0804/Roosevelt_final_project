751953a069870ac0fff95d55392575db5bcc78af
manduul0804 <manduul0804@gmail.com>
2023-03-23 04:40:43 -0500

    New updates made on DB

--- nflpickem/src/main/java/edu/roosevelt/seniorproject/nflpickem/dbsetup/InitGroups.java
+++ nflpickem/src/main/java/edu/roosevelt/seniorproject/nflpickem/dbsetup/InitGroups.java
@@ -23,8 +23,7 @@ public class InitGroups {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
+
         try {
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/nflpickem",
@@ -32,17 +31,17 @@ public class InitGroups {
 
             System.out.println("Create GROUP (not base - depends on user)");
 
-            String[] uns = {"admin", "red", "blue", "green", "yellow"};
-            String[] grps = {"bronze", "gold", "silver", "diamond", "platinum", 
-                "copper", "nickel"};
+//            String[] uns = {"admin", "red", "blue", "green", "yellow"};
+            String[] grps = {"bronze", "gold", "silver", "diamond", "platinum",
+                "copper", "nickel", "titanium", "iron", "cobalt", "tungsten", "mercury"};
 
 //            String sql = "CREATE TABLE PICKEMGROUP (";
 //            sql = sql + "NAME VARCHAR(25) PRIMARY KEY,";
 //            sql = sql + " TYPE VARCHAR(3),";
 //            sql = sql + " ADMIN VARCHAR(50),";
 //            sql = sql + " FOREIGN KEY (ADMIN) REFERENCES USER(USERNAME))";
-            String sql1 = "SELECT username "
-                    + "FROM USER;";
+            String sql1 = "SELECT username FROM USER";
+                    
 
             //Create a names list to store names from USER table from DB
             List<String> names = new ArrayList();
@@ -54,32 +53,28 @@ public class InitGroups {
 
                 while (rs.next()) {
                     String userName = rs.getString(1);
-                    System.out.println("UserName = " + userName);
                     names.add(userName);
-
                 }
 
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             System.out.println(names);
-            
-            String type = "SU";
 
             for (int i = 0; i < grps.length; i++) {
-                if((grps[i] == "diamond") && (grps[i] == "copper") ){
+                String type = new String();
+                if ((grps[i] == "diamond") || (grps[i] == "copper") || 
+                        (grps[i] == "platinum") || (grps[i] == "nickel")) {
                     type = "ATS";
-                }
-                else if(grps[i] == "platinum"){
+                } else if ((grps[i] == "titanium") || (grps[i] == "iron") || 
+                        (grps[i] == "cobalt") || (grps[i] == "tungsten")) {
                     type = "SURV";
-                }
-                else{
+                } else {
                     type = "SU";
                 }
                 String sql = "INSERT INTO PICKEMGROUP VALUES ('";
-                sql = sql + grps[i] + "','"+type+"',";
+                sql = sql + grps[i] + "','" + type + "',";
                 sql = sql + "'" + names.get(i + 1) + "')";
-                
 
                 System.out.println(sql);
 
