bf1ad79cf28e67180383d74b964f79503865e5f6
michaelruthphd <mruth@roosevelt.edu>
2023-03-23 10:39:06 -0500

    class work
    class work

--- nflpickemapp/src/app/listallusers/listallusers.component.ts
+++ nflpickemapp/src/app/listallusers/listallusers.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-listallusers',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./listallusers.component.css']
 })
 export class ListallusersComponent {
+
+  users: User[];
+
+
+  constructor(private router: Router, private userservice: UserService, private titleService: Title) { }
+
+
+  
+
+  ngOnInit(): void {
+    this.titleService.setTitle("All Users");
+
+    this.userservice.getAllUsers().subscribe(
+        data => {
+          this.users = data;
+        }, error => {
+          console.log(error.status);
+          this.router.navigateByUrl("/");
+        }
+    );
+   
+    
+    
+  }
 
 }
