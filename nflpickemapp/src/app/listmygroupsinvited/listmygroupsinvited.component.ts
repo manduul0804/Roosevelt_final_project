8dcd0f31c5472f84d30f3aa129ffc3626cabe58f
Ethan Wolfgang Seaholm <71782610+ethanseaholm@users.noreply.github.com>
2023-03-29 18:46:53 -0500

    Listmygroupsinvited Code
    Push #1

--- nflpickemapp/src/app/listmygroupsinvited/listmygroupsinvited.component.ts
+++ nflpickemapp/src/app/listmygroupsinvited/listmygroupsinvited.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Group } from '../group';
+import { PickemgroupService } from '../pickemgroup.service';
 
 @Component({
   selector: 'app-listmygroupsinvited',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./listmygroupsinvited.component.css']
 })
 export class ListmygroupsinvitedComponent {
+
+  groups: Group[]
+
+  constructor(private router: Router, private pickemgroupService: PickemgroupService, private titleService: Title) { }
+
+  unauth: boolean = false;
+
+  ngOnInit(): void {
+    this.titleService.setTitle("All Groups Invited");
+
+    this.pickemgroupService.getAllGroupsInvited().subscribe(
+      data => {
+        this.groups = data;
+      }, error => {
+        console.log(error.status);
+        if (error.status == '401') {
+          this.unauth = true;
+        } else {
+          this.router.navigateByUrl("/");
+        }
+      }
+    );
+  }
 
 }
